5618ceed83d58fe59c4138b24513d752e392d51b
LeDSinh <dinhsinh.le@outlook.com>
2018-04-03 00:26:12 +0700

    Loi JS va Dropdown

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+
+
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'clean_blogs#index'
-get  'about' 	=> 'clean_blogs#about'
-get  'post' 	=> 'clean_blogs#post'
-get  'contact' 	=> 'clean_blogs#contact'
-resources :users
+  root 'clean_blogs#index'
+  get  'about' 	=> 'clean_blogs#about'
+  get  'post' 	=> 'clean_blogs#post'
+  get  'contact' 	=> 'clean_blogs#contact'
+  #session  
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  resources :users
 end
