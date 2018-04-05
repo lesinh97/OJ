2fc7d9282bdbe24acc7ebb1db19d11b89e3c3e02
LeDSinh <dinhsinh.le@outlook.com>
2018-04-05 16:45:31 +0700

    loi k get dc user mac du da dang nhap

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,11 @@ module SessionsHelper
     def log_in(user)
     session[:user_id] = user.id
   end
+
+# Returns true if the given user is the current user.
+    def current_user?(user)
+        user == current_user
+    end
 # Tra ve mot user da login  
     def current_user
         if (user_id = session[:user_id])
@@ -37,4 +42,16 @@ module SessionsHelper
         cookies.delete(:user_id)
         cookies.delete(:remember_token)
     end
+
+    # Redirects to stored location (or to the default).
+    def redirect_back_or(default)
+        redirect_to(session[:forwarding_url] || default)
+        session.delete(:forwarding_url)
+    end
+
+  # Stores the URL trying to be accessed.
+    def store_location
+        session[:forwarding_url] = request.original_url if request.get?
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get '/edit', to: 'users#edit'
+  put '/edit', to: 'users#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'clean_blogs#index'
   get  'about' 	=> 'clean_blogs#about'
